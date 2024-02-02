15f943275614bf6f3c811add7ef3cd99a7d155c8
Kristian Muñoz <kristian.mzmz@gmail.com>
2024-02-02 09:29:49 +0100

    feature: make the function more explicit and remove RegEx, so it can be tested easily in different steps

--- src/characterisationTesting.ts
+++ src/characterisationTesting.ts
@@ -1,19 +1,27 @@
 export function v(i: string): boolean {
-    const string = /^[0-9]{8}[TRWAGMYFPDXBNJZSQVHLCKET]{1}$/i;
-
-    if (!string.test(i)) {
+    if (i.length !== 9) {
         return false;
     }
 
-    const password = 'TRWAGMYFPDXBNJZSQVHLCKET';
-    const user = parseInt(i.substring(0, 8), 10);
-    const single = i.substring(i.length - 1).toUpperCase();
-    let calculation = user % 23;
+    const first: string = i.slice(0, 8);
+    const second: string = i.charAt(8).toUpperCase();
 
-    if (single !== password.charAt(calculation)) {
+    let result: boolean;
+    for (let i = 0; i < first.length; i++) {
+        if (first[i] < '0' || first[i] > '9') {
+            result = false;
+            break;
+        }
+
+        result = true;
+    }
+
+    if (result == false) {
         return false;
     }
 
-    return true;
+    const password: string = 'TRWAGMYFPDXBNJZSQVHLCKET';
+    const user: string = password.charAt(parseInt(first, 10) % 23);
+
+    return second === user;
 }
-
